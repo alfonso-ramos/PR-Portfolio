afb6c05bd27d9a1467edb52005059636f3d63c4c
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-03-25 18:26:27 -0700

    [ADD] Creando nuevo portafolio, agregada configuracion basica de tailwind y empezando a crear al navbar

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			primary: '#31E981',
+			secondary: '#150578',
+			midnight: '#00120B',
+			white: '#F5F1ED',
+			borders: '#6B818C'
+		},
+		extend: {},
+	},
+	plugins: [],
+}
