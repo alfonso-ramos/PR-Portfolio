643fad2a7b91fd0a4aa8aa6c1b64cb54b407e6e4
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-03-27 15:43:12 -0700

    [ADD] Agregada la seccion de experiencia, falta agregar datos y despliegue a vercel para pruebas de deploy

---
+++ src/data/info.js
@@ -0,0 +1,31 @@
+export const info = {
+    experience: [
+        {
+            name: 'Hackathon: Hack the Ocean',
+            location: 'Innovaccion Virtual by Microsoft',
+            description: 'hola',
+        },
+        {
+            name: 'Hackathon: Startups Mexico by AWS Startups',
+            location: 'AWS Startups Mexico',
+            description: 'hola',
+        },
+    ],
+    education: [
+        {
+            name: 'IT Engineering',
+            location: 'Polytechnic University of Sinaloa',
+            description: 'hola',
+        },
+        {
+            name: 'CISCO IT Essentials: PC Hardware and Software',
+            location: 'CISCO NetAcad Web',
+            description: 'hola',
+        },
+        {
+            name: 'Introduction to Cybersecurity',
+            location: 'CISCO NetAcad Web',
+            description: 'hola',
+        },
+    ]
+}
