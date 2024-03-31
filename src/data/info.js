baa17cab8d6931e50c44e088ce7442d9f10575b5
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-03-30 17:30:59 -0700

    [ADD] Agregado detalles a la seccion de experiencia

--- src/data/info.js
+++ src/data/info.js
@@ -16,16 +16,24 @@ export const info = {
             name: 'IT Engineering',
             location: 'Polytechnic University of Sinaloa',
             description: 'hola',
+            startDate: '20/Aug/2023',
+            endDate: 'Present'
         },
         {
             name: 'CISCO IT Essentials: PC Hardware and Software',
             location: 'CISCO NetAcad Web',
             description: 'hola',
+            startDate: '',
+            endDate: '6/Feb/2021'
+
         },
         {
             name: 'Introduction to Cybersecurity',
             location: 'CISCO NetAcad Web',
             description: 'hola',
+            startDate: '',
+            endDate: '10/Jul/2020'
+
         },
     ],
     projects: [
